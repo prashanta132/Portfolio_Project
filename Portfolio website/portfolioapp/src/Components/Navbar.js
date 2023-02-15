eb73d60945f0c6fa7f003bc3e07a06195e72c3ba
Prashanta Patel <prashanta.kachhia@gmail.com>
2023-02-15 16:58:40 +0530

    Navbar completed

--- Portfolio website/portfolioapp/src/Components/Navbar.js
+++ Portfolio website/portfolioapp/src/Components/Navbar.js
@@ -1,33 +1,47 @@
-import React from 'react'
+import React from "react";
+import logo from "../logo.jpg"
+//react fontawsame
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"; 
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
+        
+          <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <FontAwesomeIcon icon={faBars} style={{ color:"#fff" }}/>
+            
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item ">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About Me</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Services</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">How Work</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Portfolio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+
+            </ul>
+
+
+          </div>
+        
+      </div>
+    </nav>
   )
 }
 
